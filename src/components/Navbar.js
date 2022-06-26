a2f83aa6bf0072b4c1f154972cf86205442d9c84
Mahmoud-Allaa <m.alaa50505050@gmail.com>
2022-06-26 12:00:14 +0200

    edit navebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/navbar.css";
 import logo from "./images/logo.png";
 import menu from "./images/menu.png";
@@ -7,17 +7,25 @@ import { FaChevronRight } from "react-icons/fa";
 import { NavHashLink as Link } from "react-router-hash-link";
 
 import { useTranslation } from "react-i18next";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useEffect } from "react";
 
 export default function Navbar() {
   const { t, i18n } = useTranslation();
   const language = i18n.language;
+  const [navArrow, setNavArrow] = useState("home");
 
   return (
     <div
       style={{ fontFamily: language == "ar" && "SF Arabic" }}
       className="navbar"
     >
-      <Link smooth to="/#home" className="logo">
+      <Link
+        onClick={() => setNavArrow("home")}
+        smooth
+        to="/#home"
+        className="logo"
+      >
         <img src={logo} alt="logo" />
       </Link>
 
@@ -26,35 +34,50 @@ export default function Navbar() {
           style={{ direction: language === "ar" ? "rtl" : "ltr" }}
           className="nav"
         >
-          <li>
+          <li
+            onMouseOver={() => setNavArrow("home")}
+            onMouseOut={() => setNavArrow("null")}
+          >
             <Link smooth to="/#home">
               {t("navbar.1")}
             </Link>
+            {navArrow == "home" && <KeyboardArrowDownIcon />}
           </li>
-          <li>
-            <Link
-              smooth
-              to="/#services"
-              activeClassName="selected"
-              activeStyle={{ color: "red" }}
-            >
+          <li
+            onMouseOver={() => setNavArrow("services")}
+            onMouseOut={() => setNavArrow("null")}
+          >
+            <Link smooth to="/#services">
               {t("navbar.2")}
             </Link>
+            {navArrow == "services" && <KeyboardArrowDownIcon />}
           </li>
-          <li>
+          <li
+            onMouseOver={() => setNavArrow("whyus")}
+            onMouseOut={() => setNavArrow("null")}
+          >
             <Link smooth to="/#whyus">
               {t("navbar.3")}
             </Link>
+            {navArrow == "whyus" && <KeyboardArrowDownIcon />}
           </li>
-          <li>
+          <li
+            onMouseOver={() => setNavArrow("ourpartners")}
+            onMouseOut={() => setNavArrow("null")}
+          >
             <Link smooth to="/#ourpartners">
               {t("navbar.4")}
             </Link>
+            {navArrow == "ourpartners" && <KeyboardArrowDownIcon />}
           </li>
-          <li>
+          <li
+            onMouseOver={() => setNavArrow("ourclients")}
+            onMouseOut={() => setNavArrow("null")}
+          >
             <Link smooth to="/#ourclients">
               {t("navbar.5")}
             </Link>
+            {navArrow == "ourclients" && <KeyboardArrowDownIcon />}
           </li>
         </ul>
         <li className="contact-btn">
